ae589a6172aa77c695b9db8db2852928629331c1
voluntas <nakai@shiguredo.jp>
2026-06-07 00:45:58 +0900

    0036 src/sys.rs の #[allow(...)] を #[expect(...)] に変更する
    - AGENTS.md の lint 抑制規約に従い allow → expect に置き換え

--- src/sys.rs
+++ src/sys.rs
@@ -1,8 +1,8 @@
-#![allow(non_upper_case_globals)]
-#![allow(non_camel_case_types)]
-#![allow(non_snake_case)]
-#![allow(dead_code)]
-#![allow(clippy::all)]
+#![expect(non_upper_case_globals)]
+#![expect(non_camel_case_types)]
+#![expect(non_snake_case)]
+#![expect(dead_code)]
+#![expect(clippy::all)]
 
 include!(concat!(env!("OUT_DIR"), "/metadata.rs"));
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
